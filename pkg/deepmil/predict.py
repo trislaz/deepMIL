17c502520b202d25d0730321f02fb4d4fc0de923
Tristan LAZARD <tlazard@thalassa.cm.cluster>
2020-08-03 15:35:56 +0200

    Ajout matrice de confusion

--- pkg/deepmil/predict.py
+++ pkg/deepmil/predict.py
@@ -1,6 +1,8 @@
 """Just predicts, given a set of WSI.
 """
 import numpy as np
+import seaborn as sns
+from sklearn import metrics
 import pandas as pd
 import torch
 import pandas as pd
@@ -10,20 +12,6 @@ from .arguments import get_arguments
 from .models import DeepMIL
 from .dataloader import EmbededWSI, Dataset_handler
 from collections import MutableMapping
-
-def update_confusion(confu_dict, y_hat, y):
-    if y == 1:
-        if y_hat == 1:
-            confu_dict['TP'] += 1
-        else:
-            confu_dict['FN'] += 1
-    else:
-        if y_hat == 1:
-            confu_dict['FP'] += 1
-        else:
-            confu_dict['TN'] += 1
-    return confu_dict
-
 
 def load_model(config, model_path=None, dataset=None, table_data=None):
     args = get_arguments(train=False, config=config)
@@ -40,7 +28,6 @@ def load_model(config, model_path=None, dataset=None, table_data=None):
     data = Dataset_handler(args, predict=True)
     dataloader = data.get_loader(training=False)
     df = dataloader.dataset.table_data
-    confusion_dict = {'TP': 0, 'TN': 0, 'FP': 0, 'FN': 0}
     results = []
     model.network.eval()
     for o, (x, y) in enumerate(dataloader):
@@ -48,7 +35,12 @@ def load_model(config, model_path=None, dataset=None, table_data=None):
         id_im = os.path.splitext(os.path.basename(dataloader.dataset.files[o]))[0] 
         serie = df[df['ID'] == id_im].to_dict('records')[0]
         success = y_hat[1].item() == y.item()
-        results.append({'prediction': y_hat[1].item(), 'gt': y.item(), 'index':o,'success': success}.update(serie))
-        if args.test_fold == serie['test']:
-            confusion_dict = update_confusion(confu_dict = confusion_dict, y_hat=y_hat, y=y)
-    return pd.DataFrame(results), confusion_dict
+        r = {'prediction': y_hat[1].item(), 'gt': y.item(), 'index':o,'success': success}
+        r.update(serie) 
+        results.append(r)
+    results = pd.DataFrame(results)
+    results_test = results[results['test'] == args.test_fold]
+    predicted_labels = results_test['prediction'].values
+    true_labels = results_test['gt'].values
+    confusion_mat = metrics.confusion_matrix(y_true=true_labels, y_pred=predicted_labels)
+    return results, confusion_mat, dataloader.dataset.target_correspondance 
--- scripts/predict.py
+++ scripts/predict.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 from deepmil.predict import load_model
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 parser = ArgumentParser()
 parser.add_argument("--config", type=str, help='path to the config file -.yaml format-')
@@ -7,5 +9,11 @@ parser.add_argument("--model_path", type=str, help='path to the model to use')
 parser.add_argument("--out_name", type=str, help='name of the output file', default='prediction_output')
 args = parser.parse_args()
 
-df = load_model(config=args.config, model_path=args.model_path)
+df, confusion_mat, target_correspondance = load_model(config=args.config, model_path=args.model_path)
+heatmap = sns.heatmap(confusion_mat, annot=True, cmap=plt.cm.Blues, normaliz)
+heatmap.yaxis.set_ticklabels(target_correspondance, rotation=0, ha='right', fontsize=12)
+heatmap.xaxis.set_ticklabels(target_correspondance, rotation=45, ha='right', fontsize=12)
+plt.xlabel('predicted label')
+plt.ylabel('true label')
+plt.savefig(args.out_name + '_confu_test.jpg')
 df.to_csv(args.out_name+'.csv', index=False)
